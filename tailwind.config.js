c5837db7d68f4078cb4901d51dfa76b820088050
Andrew Raphael <andrewrgpires@gmail.com>
2024-10-10 08:40:02 -0300

    add fonts and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,18 @@
 tailwind.config = {
     theme: {
         fontFamily: {
-
+            'workSans':['Work Sans', 'sans-serif'],
         },
         extend: {
             backgroundImage: {
                 'imgDesktop': "url('./assets/images/background-pattern-desktop.svg')",
                 'imgMobile': "url('./assets/images/background-pattern-mobile.svg')",
             },
+            colors: {
+                'lightPink':'#F9F0FF',
+                'grayishPurple':'#8C6991',
+                'darkPurple':'#2F1533',
+            },       
         }
     }
 }
