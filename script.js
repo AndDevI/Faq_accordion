b631d2ff9efaed2d5097e25015fdd1f34e64fd02
Andrew Raphael <andrewrgpires@gmail.com>
2024-10-10 13:29:59 -0300

    replacing the image with js

--- script.js
+++ script.js
@@ -3,13 +3,16 @@ const accordionHeaders = document.querySelectorAll('.accordionHeader');
 accordionHeaders.forEach(header => {
     header.addEventListener('click', () => {
         const isActive = header.classList.contains("active");
-        const accordionBody = document.querySelectorAll('.accordionBody')
+        const icon = header.querySelector('img'); 
 
         accordionHeaders.forEach(otherHeader => {
             otherHeader.classList.remove("active");
             const body = otherHeader.nextElementSibling;
             body.style.maxHeight = '0';
             body.classList.remove('mt-5');
+            
+            const otherIcon = otherHeader.querySelector('img');
+            otherIcon.setAttribute('src', 'assets/icons/icon-plus.svg');
         });
 
         if (!isActive) {
@@ -17,6 +20,7 @@ accordionHeaders.forEach(header => {
             const accordionBody = header.nextElementSibling;
             accordionBody.style.maxHeight = `${accordionBody.scrollHeight}px`; 
             accordionBody.classList.add('mt-5');
+            icon.setAttribute('src', 'assets/icons/icon-minus.svg');
         }
     });
 });
