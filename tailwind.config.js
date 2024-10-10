67edfdf5aee29fff4fd4e8b84e3bcf744ec0889b
Andrew Raphael <andrewrgpires@gmail.com>
2024-10-10 08:31:45 -0300

    starting and config project

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+    theme: {
+        fontFamily: {
+
+        },
+        extend: {
+            backgroundImage: {
+                'imgDesktop': "url('./assets/images/background-pattern-desktop.svg')",
+                'imgMobile': "url('./assets/images/background-pattern-mobile.svg')",
+            },
+        }
+    }
+}
